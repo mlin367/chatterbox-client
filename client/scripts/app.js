745b96dc20c5fd7677de3fd9e173e46d05e46699
Matthew Lin <mlin367@yahoo.com>
2018-09-30 16:43:06 -0700

    Implement functionality for rooms and friends list

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,18 +1,32 @@
 // YOUR CODE HERE:
 let app = {};
 app.recentId;
+//Most recent data from fetch whenever it is invoked
+app.data;
 app.server = 'http://parse.la.hackreactor.com/chatterbox/classes/messages'
 app.init = function() {
-  
 };
+//Default room will always be lobby and it will always exist, for now...
+app.currentRoom = 'lobby';
+app.rooms = ['lobby'];
 app.messages = [];
 app.friendsList = [];
 
 app.renderMessage = function(message) {
-  $('#chats').prepend(`<p> <span class="username">${message.username}</span>: ${message.text}</p>`);
-  let current = $('.username')[0];
-  $(current).on('click', () => {
-    app.handleUsernameClick(message.username)});
+  if (!!(message.text && message.username && !app.messageChecker(message))) {
+    if(app.friendsList.includes(message.username)) {
+      $('#chats').prepend(`<p> <span class="username">${message.username}</span>: <strong> ${message.text} </strong></p>`);
+    } else {
+      $('#chats').prepend(`<p> <span class="username">${message.username}</span>: ${message.text}</p>`);
+    }
+    let current = $('.username')[0];
+      $(current).on('click', () => {
+        app.handleUsernameClick(message.username)
+      });
+  } 
+  if(app.messageChecker(message)) {
+    console.log(`${message.username} tried to send "${message.text}" =\\`)
+  }
 };
 
 app.clearMessages = function() {
@@ -20,7 +34,17 @@ app.clearMessages = function() {
 };
 
 app.renderRoom = function(room) {
-  $('#roomSelect').prepend(`<p> ${room} </p>`);
+  $('#selectDrop').prepend(`<option value="${room}">${room}</option>`);
+};
+
+//Renders messages for current room only
+app.handleDropDownClick = function(room, data) {
+  app.clearMessages();
+  for(let i = data.length - 1; i >= 0; i--) {
+    if(data[i].roomname === room) {
+      app.renderMessage(data[i]);
+    }
+  }
 };
 
 app.handleUsernameClick = function(name) {
@@ -28,6 +52,7 @@ app.handleUsernameClick = function(name) {
     console.log(`${name} already added.`)
   } else {
     app.friendsList.push(name);
+    $('#friendsList').append('</br>' + name);
     console.log(`${name} added.`)
   }
 };
@@ -41,6 +66,7 @@ app.send = function(message) {
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
+        $('img').fadeToggle();
         console.log('chatterbox: Message sent');
       },
       error: function (data) {
@@ -56,12 +82,35 @@ app.handleSubmit = function(e) {
   var message = {
     username: window.location.search.split('=')[1],
     text: content,
-    roomname: 'lobby'    
-  }
-  app.send(message)
+    roomname: app.currentRoom
+  };
+  $('img').toggle();
+  app.send(message);
 };
 
+//Event handlers for submit and drop down
 $('#send').on('submit', app.handleSubmit);
+$('#selectDrop').on('change', () => {
+  $('img').toggle();
+  let value = $( "select option:selected" ).text();
+  if(value === 'Create Room') {
+    let newRoom = prompt('Enter a name for the new room: ');
+    if(newRoom === null || newRoom === "") {
+      alert('Room creation cancelled');
+      $('#selectDrop').val(app.currentRoom).trigger('change');
+    } else {
+      app.rooms.push(newRoom);
+      app.renderRoom(newRoom);
+      app.currentRoom = newRoom;
+      $('#selectDrop').val(newRoom).trigger('change');
+    }
+  } else {
+    app.currentRoom = value;
+  }
+  app.handleDropDownClick(app.currentRoom, app.data['results']);
+  $('img').fadeToggle();
+})
+
 
 app.fetch = function() {
   $.ajax({
@@ -71,27 +120,16 @@ app.fetch = function() {
       type: 'GET',
       contentType: 'application/json',
       success: (data) => {
-        // if (!app.recentId) {
-        //   app.recentId = data['results'][9].objectId;
-        // }
-        //   // console.log(data['results'])
-        // for (let element of data['results']) {
-        //   if (element.objectId === app.recentId) {
-        //     break;
-        //   }
-        //   console.log(element);
-        //   if (element.text && element.username) app.renderMessage(element);
-        //   app.recentId = element.objectId;
-        //   console.log(element['username'] + ' sent a message');
-        // }
-        let recentMessage = data['results'][0]
-        if(app.recentId !== recentMessage.objectId) {
-          // if (app.messageChecker(data['results'][0]))
-          if (!!(recentMessage.text && recentMessage.username && !app.messageChecker(recentMessage))) {
-            app.renderMessage(recentMessage);
+        //Tests if the data has changed and only run code below if it has
+        if(JSON.stringify(app.data) !== JSON.stringify(data)) {
+          app.data = data;
+          for(let message of data['results']) {
+            if(!app.rooms.includes(message.roomname)) {
+              app.rooms.push(message.roomname);
+              app.renderRoom(message.roomname);
+            }
           }
-          app.recentId = recentMessage.objectId;
-          console.log(recentMessage['username'] + ' sent a message');
+          app.handleDropDownClick(app.currentRoom, data['results'])
         }
       },
       error: function (data) {
@@ -105,4 +143,6 @@ app.messageChecker = function (message) {
   return JSON.stringify(message).includes('<script>');
 };
 
-setInterval(app.fetch, 100);
+app.fetch();
+setInterval(app.fetch, 1000);
+
